103439f8043d614b572b8e49f004c37d55c640ed
aswnssWsl <aswnss.m@gmail.com>
2025-03-30 10:18:33 +0000

    feat: Calculate the age from Dob

--- clinic_care/clinic_care/doctype/patient/patient.js
+++ clinic_care/clinic_care/doctype/patient/patient.js
@@ -1,8 +1,35 @@
 // Copyright (c) 2025, Soft Served Web and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Patient", {
-// 	refresh(frm) {
+frappe.ui.form.on("Patient", {
+    onload: function (frm) {
+        if (frm.doc.dob) {
+            $(frm.fields_dict['age_html'].wrapper).html(`${__('AGE')} : ${get_age(frm.doc.dob)}`);
+        } else {
+            $(frm.fields_dict['age_html'].wrapper).html('');
+        }
+    }
+});
 
-// 	},
-// });
+frappe.ui.form.on('Patient', 'dob', function (frm) {
+    if (frm.doc.dob) {
+        let today = new Date();
+        let birthDate = new Date(frm.doc.dob);
+        if (today < birthDate) {
+            frappe.msgprint(__('Please select a valid Date'));
+            frappe.model.set_value(frm.doctype, frm.docname, 'dob', '');
+        } else {
+            let age_str = get_age(frm.doc.dob);
+            $(frm.fields_dict['age_html'].wrapper).html(`${__('AGE')} : ${age_str}`);
+        }
+    } else {
+        $(frm.fields_dict['age_html'].wrapper).html('');
+    }
+});
+
+let get_age = function (birth) {
+    let birth_moment = moment(birth);
+    let current_moment = moment(Date());
+    let diff = moment.duration(current_moment.diff(birth_moment));
+    return `${diff.years()} ${__('Year(s)')} ${diff.months()} ${__('Month(s)')} ${diff.days()} ${__('Day(s)')}`
+};
